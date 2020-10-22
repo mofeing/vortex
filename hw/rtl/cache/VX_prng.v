f6dff880f9cab6101f9ea9c07f52ce88d5d3d2dc
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-10-22 20:00:59 +0000

    Implement PRNG module
    Pseudo-Random Number Generator based on Linear-Feedback-Shift-Registers and XNORs.

--- hw/rtl/cache/VX_prng.v
+++ hw/rtl/cache/VX_prng.v
@@ -1,28 +1,41 @@
 module VX_prng #(
 	parameter SEED = 0,
-	parameter NBITS = 8,
-	parameter NNUM = 16,
+
+	// #randomness bits
+	parameter NBITS = 168,
+
+	// #output bits
+	parameter NNUM = 2,
 )(
 	input wire clk,
 	input wire reset,
 
-	output wire num[NNUM-1:0];
-	// output wire other[NBITS-1:NNUM];
+	output wire rnd[NNUM-1:0];
 );
 
-reg [NNUM-1:0] counter;
+reg [NBITS-1:0] lsfr;
+wire [NNUM-1:0][3:0] xnor_in;
+wire [NNUM-1:0] xnor_out;
+
+assign xnor_out = rnd;
+
+for (i = 0; i < NNUM; i++) begin
+	assign xnor_in[i][0] = lsfr[NBITS - NNUM + i];
+	assign xnor_in[i][1] = lsfr[NBITS - NNUM + i - 2];
+	assign xnor_in[i][2] = lsfr[NBITS - NNUM + i - 15];
+	assign xnor_in[i][3] = lsfr[NBITS - NNUM + i - 17];
+
+	assign xnor_out[i] = xnor(xnor_in[i][3:0]);
+end
 
 always @(posedge clk) begin
-	if (reset) begin
-		counter = SEED;
+	if (reset) lsfr = SEED;
+	else begin
+		for (i = NBITS-1; i >= NNUM; i--)
+			lsfr[i] = lsfr[i-1];
+		for (i = NNUM-1; i >= 0; i--)
+			lsfr[i] = xnor_out[i];
 	end
 end
-
-always @(posedge clk) begin
-	counter <= counter + 1;
-end
-
-// 53831 is just a prime number
-num = (53831 * counter) % (2**NBITS);
 
 endmodule
